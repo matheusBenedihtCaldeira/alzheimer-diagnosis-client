66904a4c7b41d215dbbf5d2bd0677bf05f2cb28b
bene <matheusbenediht@gmail.com>
2023-11-22 15:25:44 -0300

    Update: Tornando rota de registro de pacientes privada

--- src/app/(auth-routes)/login/page.tsx
+++ src/app/(auth-routes)/login/page.tsx
@@ -83,7 +83,7 @@ export default function login() {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-sky-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-800"
+              className="flex w-full justify-center rounded-md bg-sky-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-800 transition duration-250 ease-out md:ease-in"
             >
               Sign in
             </button>
